95691e7f56da7fee8424353a8aa4eaa5eb5d2edf
santiago iglesias meeks <santiago@iglesiasmeeks.com>
2023-04-10 20:37:50 -0300

    templates

--- proyecto_sim_inicio/urls.py
+++ proyecto_sim_inicio/urls.py
@@ -1,6 +1,11 @@
+from django.contrib import admin
 from django.urls import path
 from proyecto_sim_inicio import views
 
 urlpatterns = [
-    path('', views.inicio)
+    path ('', views.inicio),
+    path ('mostrar_fecha/', views.mostrar_fecha),
+    path ('saludo/<str:nombre>/<str:apellido>/', views.saludo),
+    path ('mi_template/', views.mi_template),
+    # path ('admin/', admin.site.urls)
 ]
--- proyecto_sim_inicio/views.py
+++ proyecto_sim_inicio/views.py
@@ -1,4 +1,29 @@
+from datetime import datetime
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template, Context
 
 def inicio(request):
     return render(request, 'proyecto_sim_inicio/inicio.html')
+
+def mostrar_fecha(request):
+    dt = datetime.now()
+    dt_formateado = dt.strftime("%A %d %B %Y %I:%M")
+    return HttpResponse(f' <p>{dt_formateado} </p>')
+
+def saludo(request, nombre, apellido):
+    return HttpResponse(f'<h1>Hola {nombre} {apellido}</h1>')
+
+def mi_template(request):
+    
+    archivo = open(r'C:\Datos\CODERHOUSE\entrega_3\templates\mi_template.html')
+    
+    template = Template(archivo.read())
+    
+    archivo.close()
+    
+    contexto = Context()
+    
+    template_renderizado = template.render(contexto)
+    
+    return HttpResponse(template_renderizado)
