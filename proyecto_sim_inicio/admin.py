4421db2571d74e09b175068a5374874c941b624b
santiago iglesias meeks <santiago@iglesiasmeeks.com>
2023-04-12 02:33:25 -0300

    modificacion de admin y models

--- proyecto_sim_inicio/admin.py
+++ proyecto_sim_inicio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from proyecto_sim_inicio import Persona, Empresa
+
 
 # Register your models here.
+
+admin.site.register(Persona)
+admin.site.register(Empresa)
--- proyecto_sim_inicio/models.py
+++ proyecto_sim_inicio/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+
+class Empresa(models.Model):
+    nombre = models.CharField(max_length=20)
+    cuit = models.IntegerField(max_length=11)
+    
+class Rubro(models.Model):
+    categoria = models.CharField(max_length=20)
+    marca = models.CharField(max_length=20)
+    
+class Zona(models.Model):
+    Provincia = models.CharField(max_length=20)
+    codigo_postal = models.IntegerField(max_length=11)
